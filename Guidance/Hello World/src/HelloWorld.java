bd4185e3da7f20821fa4c5d83259bfa2cbb9fba9
KirchnerMax <kirchnermax98@googlemail.com>
2019-10-27 13:07:21 +0100

    [GUI] JavaDov-Test + Missionsübersicht V2.0

---
+++ Guidance/Hello World/src/HelloWorld.java
@@ -0,0 +1,23 @@
+import lejos.nxt.Button;
+
+@SuppressWarnings("javadoc")
+public class HelloWorld {
+
+/**
+ * This is my first project. I want to write HelloWorld!
+ *  Test
+ *  
+ *  @author Kirchner_Max
+ *  @version 1.0
+ *  @param args This is the description. 
+ *   
+ */
+	
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Button.waitForAnyPress();
+    }
+}
+
+
+

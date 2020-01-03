e368ae5d3aad9cb6d84d8d95f457bec747813307
Sebastian Schwabe <sebastian.schwabe@turag.de>
2020-01-03 14:19:39 +0100

    [PER]sharp sample

--- Perception/Python_skripte/sharp_auswertung_clean.py
+++ Perception/Python_skripte/sharp_auswertung_clean.py
@@ -17,16 +17,17 @@ for i in range(10,81): #itterate thru all files
    j=0
    k=0
    l=0
-   filename = os.path.join(dirname, '../Sharp Sensoren/sample_sharp_s4/output_'+str(i)+'cm.txt')
+   filename = os.path.join(dirname, '../Sharp Sensoren/sharp_1_3_dig/output_'+str(i)+'cm.txt')
    with open(filename,"r") as file: #open i'ten file 
         if file.mode == 'r': #check if file is readable
            for j, line in enumerate(file): #itterate thru file i 
 
-               if j > 15 and k < 15:     #if flag is set and number of lines < 15 true 
+               if (j > 0 and k < 16):     #if flag is set and number of lines < 15 true 
           
                     try:                   #exeption for convertion from string to float
                          t = float(file.readline())
-                         sample_array[i-10,k] = t
+                         r = round(t,3)
+                         sample_array[i-10,k] = r
                          #print(t)
                          k+=1   
                     except ValueError:
@@ -58,18 +59,35 @@ for i in range(0,71):         #calculate expected value and variance and print t
 
 
 for i in range(0,71):         #generate x values 10-80
-     gauss_array[i,0] = i+10
+     gauss_array[i,0] = (i+10)*10
 
 
 plt.errorbar(gauss_array[:, 0],gauss_array[:, 1],yerr= gauss_array[:, 2],fmt='o')  #plot dots with error bars 
-p = np.polyfit(gauss_array[:, 0],gauss_array[:, 1],6)                              #make a polynomial fit of 6 degree
+p = np.polyfit(gauss_array[:, 0],gauss_array[:, 1],5)                              #make a polynomial fit of 5 degree
 print(p)                                                                           #print coifficions
 f = np.poly1d(p)                                                                   #build function with coifficions
 plt.plot(gauss_array[:, 0],f(gauss_array[:, 0]), 'r-',label="Polyfit")             #plot polyfit function
-plt.xlabel("Distance in cm")
+plt.xlabel("Distance in mm")
 plt.ylabel("Spannung in V")
 red_patch = mpatches.Patch(color='red', label='fit')
 plt.legend(handles=[red_patch])
+
+
+#plot Distance(voltage)
+plt.figure()
+u = np.polyfit(gauss_array[:,1],gauss_array[:,0],5)
+print(u)
+b = np.poly1d(u)
+x1 = np.arange(0.1, 1.22, 0.02)
+plt.plot(x1,b(x1),'-',color='orange')
+plt.ylabel("Distance in cm")
+plt.xlabel("Spannung in V")
+red_patch = mpatches.Patch(color='orange', label='inv fit')
+plt.legend(handles=[red_patch])
+
+
+
+
 plt.show()                                                                         #show plots
 
 

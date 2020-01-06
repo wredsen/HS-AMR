f1806c54f30276abaf1ab6d48d0c4ef9155c664b
KonstantinKuhl <charles.cool28@gmail.com>
2020-01-06 19:03:24 +0100

    [NAV] Ecken werden bei Anfahrt teils nicht mehr überschrieben!

--- shared_workspace/hsamr1/src/parkingRobot/hsamr1/NavigationAT.java
+++ shared_workspace/hsamr1/src/parkingRobot/hsamr1/NavigationAT.java
@@ -501,7 +501,7 @@ public class NavigationAT implements INavigation{
 		
 		double parkingSlotLength = Math.abs(frontValue-backValue);
 		
-		if (parkingSlotLength>0.30) return true;
+		if (parkingSlotLength>0.45) return true;
 		else return false;
 	}
 	
@@ -710,11 +710,11 @@ public class NavigationAT implements INavigation{
 			switch(lastCornerNumber)
 				{
 				case 0: 
-						//this.pose.setLocation((float)0.00,(float)0.00);
+						//if (Guidance.getAnfahrt() == false)	this.pose.setLocation((float)0.00,(float)0.00);
 						nextCornerNumber = 1;
 					break; 
 				case 1: 
-						//this.pose.setLocation((float)1.80,(float)0.00);
+						//if (Guidance.getAnfahrt() == false)	this.pose.setLocation((float)1.80,(float)0.00);
 						nextCornerNumber = 2;
 						foundBackBoundary = false;
 					break; 
@@ -728,7 +728,7 @@ public class NavigationAT implements INavigation{
 						nextCornerNumber = 4;
 					break;
 				case 4:
-						//this.pose.setLocation((float)1.50,(float)0.30);
+						//if (Guidance.getAnfahrt() == false)this.pose.setLocation((float)1.50,(float)0.30);
 						nextCornerNumber = 5;
 					break;
 				case 5:
